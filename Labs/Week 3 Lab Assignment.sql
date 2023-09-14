9d8fd7c916fd14405bcc04fbb0cf8dc2b3a74038
seanscott1991 <44501856+seanscott1991@users.noreply.github.com>
2023-09-14 17:32:37 -0400

    Add files via upload

---
+++ Labs/Week 3 Lab Assignment.sql
@@ -0,0 +1,111 @@
+-- ------------------------------------------------------------------------------------------- --
+-- ---------------------------------------  Week 3 Lab  -------------------------------------- --
+-- ------------------------------------------------------------------------------------------- --
+
+-- Instructions --
+
+-- Use the tpch relational dataset to answer the following questions. Write your code in this
+-- file after each corresponding question and include a comment with the answer to the question
+-- if applicable.  Code should use syntax best practices.  Submit code at end of lab session.
+
+
+
+-- About the tpch database --
+
+-- The tpch relational dataset contains data on 1.5 million orders for 'parts'.  The orders table 
+-- contains data on each order.  The line item table contains data on the specific parts included
+-- in each order.  The customer, nation, part, partsupp, region, and supplier tables all contain
+-- additional data that can be related back to the orders and line items. 
+
+
+
+
+
+
+
+-- 1. Return all parts with a below average retail price --
+
+
+
+
+
+
+-- 2. Return all customers who have an account balance smaller than the smallest order with status of 'P' in the data base --
+
+
+
+
+
+
+
+-- 3. Return all columns from the customers table as well as a column that categorizes account balances into small, medium, large --
+-- and extra large balances.  Extra large balances are considered to be over $8k, large balances are considered to be larger than $6k --
+-- and less than or equal to $8k, medium balances are considered to be larger than $3k and less than or equal to $6k, and small orders are --
+-- considered to be $3k or less. --
+
+
+
+
+
+-- 4. Using a sub query return a list of all nations who have a customer with a '988' area code and an account balance of at least $9,500  --
+
+			
+		
+
+
+
+
+
+-- 5. Which customer market segments have a higher total account balance than the automobile and building segments? --
+
+
+
+
+
+
+
+-- 6. Write a query that show percent of total orders for each order priority (results should be on one row) --
+
+
+
+
+
+
+
+-- 7. Write a query that show percent of total order value for each order priority (results should be on one row) --
+
+
+
+
+
+
+
+-- 8. Rewrite your query from #6 so that the data is now at the customer number level  --
+
+
+
+
+
+
+-- 9. Join your results from #8 to the customer table (all customers should be in result set) --
+
+
+
+
+
+
+-- 10. Rewrite the query from the previous problem but this time use a common table expression --
+
+
+
+
+
+
+
+	
+	
+
+
+
+
+
