060672803efd67172db0bfa44bccdeebc3f408f8
seanscott1991 <44501856+seanscott1991@users.noreply.github.com>
2025-10-22 09:46:10 -0400

    Add files via upload

---
+++ Assignments/STA 690 Midterm Assignment.sql
@@ -0,0 +1,152 @@
+-- ------------------------------------------------------------------------------------------- --
+-- -------------------------------  STA 690 Midterm Assignment  ------------------------------ --
+-- ------------------------------------------------------------------------------------------- --
+
+-- Instructions --
+
+-- Use the Credit relational dataset to answer the following questions. Write your code in this
+-- file after each corresponding question and include a comment with the answer to the question
+-- if applicable.  Code should use syntax best practices.  
+
+
+
+
+
+
+
+-- 1. Return all columns from the charge table for charges less than $300 --
+
+
+    
+
+
+
+-- 2. Return count of members who made a category 3 charge in September --
+
+  
+  
+  
+  
+  
+ -- 3. Among charges from categories 6, 7, 8, and 9 return the average and total charge amounts from the charge table, give the columns aliases --   
+
+  
+  
+  
+  
+    
+ -- 4. Return count of all members whose first name ends with 'EY' --   
+
+    
+
+
+
+
+-- 5. Return average charge amount by month from the charge table --
+
+
+
+
+
+
+-- 6. Return average charge amount by month from the charge table among charges with category 3, 4, and 6 --
+
+
+
+
+
+
+-- 7. Return average charge amount by month from the charge table among charges with category 3, 4 and 6. --
+-- Include only months that have an average charge amount higher than the entire table's average charge amount --
+
+
+
+
+
+
+-- 8. Return all columns for charges in September.  Comment how many charges are included in result set --
+
+
+
+
+
+
+-- 9. Update your question #8 query to include a left join with the statement table --
+-- Comment how many rows are included in your result set --
+
+
+
+
+
+
+-- 10. Update your question #8 query to include an inner join with the statement table --
+-- Comment how many rows are included in your result set and why it differs from your row count in question 9 --
+
+
+
+
+
+
+-- 11. Return average charge amount per provider region (include region name) ordered from lowest to highest average charge amount --
+
+
+
+
+
+
+-- 12. Return total category 10 charges per member region (include region name) ordered from most to least charges --
+
+
+
+
+
+
+-- 13. Return all payments that are larger than the largest charge in the charge table and have a payment date in October --
+
+
+
+
+
+
+-- 14. Return all provider names who have total charge amounts lower than providers 'Prov. Boston Ma' and 'Prov. Apex Petr' --
+
+
+
+
+
+
+-- 15. Using an inner join and a sub query filter the charge table to only charges linked with providers with 'Famous' in their name --
+
+
+
+
+
+
+-- 16. Return total charge amount per month, please have results in one row --
+
+	
+
+
+
+
+-- 17. Convert your results to show percentage of total charges for each month.  Round results to 1 decimal place --
+
+
+
+
+
+
+-- 18. Convert results to show the same information but at the provider_no level and join your results to the provider table using a sub query --
+
+
+
+
+
+
+-- 19. Rewrite the same query from problem 18 but leverage common table expressions --
+
+
+
+
+
+
