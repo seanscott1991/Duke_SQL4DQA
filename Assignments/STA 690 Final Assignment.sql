c7bfdbd3894412613d544845be69a20365f13f9b
seanscott1991 <44501856+seanscott1991@users.noreply.github.com>
2026-09-18 17:04:50 -0400

    Add files via upload

---
+++ Assignments/STA 690 Final Assignment.sql
@@ -0,0 +1,67 @@
+-- ------------------------------------------------------------------------------------------- --
+-- -------------------------------   STA 690 Final Assignment   ------------------------------ --
+-- ------------------------------------------------------------------------------------------- --
+
+-- Instructions --
+
+-- Write your code in this file after each corresponding question and include a comment with the answer --
+-- to the question if applicable.  Code should use syntax best practices.  Submit code by EOD 10/14/22. --
+
+
+
+
+
+
+-- 1. Use the Credit database. For each region, return which member owes the credit card company the most and the --
+-- amount that they owe.  Amount owed is calculated as total charges minus total payments, which are found in the --
+-- payment table.  If a member is not present in the payments table then they have not made any payments and they --
+-- owe the full amount of their charges.  Final query should include a region name column, a member first name --
+-- column, a member last name column, and an amount owed column. --
+
+
+
+
+
+
+
+
+
+-- 2. Use the Credit database.  Please return the 5 days that had the highest week over week charges increase. --
+-- This is calculated as the difference between a day's total charges minus the total charges from the day a week prior. --
+-- We are only interested in charges from members that are associated with a corporation. Output should be ordered by week over --
+-- week charges increase descending and include a date column and a column for the week over week charges increase amount. --
+
+
+
+
+
+
+
+
+
+-- 3. Use the credit database. The "Corp. Imperial RestaurantsAg." corporation is interested in looking at all charges they have --
+-- made in the meals category.  They would like to know, on average, how many days pass between days that included a meals charge --
+-- of theirs. Their meals related charges tend to come in bunches, so they are not interested in gaps between charges that --
+-- occured on the same day, only the gaps between the days themselves. --
+
+
+
+
+
+
+
+
+
+-- 4. Use the credit database.  The credit card company is interested in analyzing basic differences between charges that have been --
+-- connected to a statement versus charges that have not.  Please return total charges and average charge amount for both groups of --
+-- charges.  Output should include 3 columns and 2 rows.  A column containing a categorical variable seperating the two groups, a --
+-- total charges column and an average charge amount column. --
+
+
+
+
+
+
+
+
+
